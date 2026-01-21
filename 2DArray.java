6e03dc224d6fa4871bf98596aa0ce1bf632061f2
Pratik Shedule <pgshedule@gmail.com>
2026-01-21 11:07:24 +0530

    2DArray

---
+++ 2DArray.java
@@ -0,0 +1,11 @@
+class TwoDArray {
+    public static void main(String[] args){
+        int [][] numbers = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        System.out.println(numbers[0][0]);
+        System.out.println(numbers[1][2]);
+    }
+}
